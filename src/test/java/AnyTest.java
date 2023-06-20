dc013ed07c3c43c9f17d9fd8ba09895a62ab5300
Raimo Radczewski <rradczewski@users.noreply.github.com>
2023-06-20 10:51:59 +0000

    Add a complicated codebase

--- src/test/java/AnyTest.java
+++ src/test/java/AnyTest.java
@@ -5,11 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AnyTest {
 
     @Test
-    void it_fails() {
-        assertThat(false).isTrue();
-    }
-
-    @Test
     void it_succeeds() {
         assertThat(true).isTrue();
     }
